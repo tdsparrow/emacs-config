3fdcdecec7fdb02e49ea1761dc5d43f5813a8993
erenlii <ren.li@ericsson.com>
2015-06-09 18:32:27 +0800

    more mode init file

---
+++ erlang/el-get-setup/init-company-mode.el
@@ -0,0 +1,1 @@
+../../init.d/init-company-mode.el
--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -3,5 +3,7 @@
 (helm-mode 1)
 (helm-autoresize-mode 1)
 (setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 (provide 'init-helm)
