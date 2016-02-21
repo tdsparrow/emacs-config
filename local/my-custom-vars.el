6859dfdb97e9080c2c3db0dd0b5cec26c7d83b79
tdsparrow <sqallowlee@gmail.com>
2016-02-21 16:09:50 +0800

    add ggtags

---
+++ local/my-custom-vars.el
@@ -0,0 +1,3 @@
+(setq my-pkg-list '(go-mode helm yaml-mode monokai-theme elpy helm-gtags))
+
+(provide `my-custom-vars)
--- local/my-custom.vars.el
+++
@@ -1,3 +0,0 @@
-(setq my-pkg-list '(go-mode helm yaml-mode monokai-theme elpy))
-
-(provide `my-custom-vars)
