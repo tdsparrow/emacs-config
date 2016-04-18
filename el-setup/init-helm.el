8dfdaafe2bb10a7aba8de9fa5c86df820ac32738
tdsparrow <sqallowlee@gmail.com>
2016-04-18 10:45:26 +0800

    enable helm-find-files

--- el-setup/init-helm.el
+++ el-setup/init-helm.el
@@ -5,5 +5,6 @@
 (setq helm-M-x-fuzzy-match t)
 (setq helm-buffers-fuzzy-matching t)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (provide 'init-helm)
