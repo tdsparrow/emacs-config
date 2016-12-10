68a98040d628e0a25a499336bc27bc8d7e5b4bea
tdsparrow <sqallowlee@gmail.com>
2016-12-10 11:39:03 +0800

    add web-mode and linum

--- init.d/init-system.el
+++ init.d/init-system.el
@@ -1,2 +1,4 @@
+(global-linum-mode 1)
+(setq linum-format "%d ")
 
 (provide 'init-system)
--- local/my-custom-vars.el
+++ local/my-custom-vars.el
@@ -1,4 +1,6 @@
 (require 'my-local-vars nil 'noerror)
 (setq my-pkg-list '(go-mode helm yaml-mode monokai-theme elpy helm-gtags web-mode))
 
+(add-hook 'isearch-update-post-hook 'redraw-display)
+
 (provide `my-custom-vars)
