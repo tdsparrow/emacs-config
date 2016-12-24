5855935e36bc94978fdf87365d2ccdba6804dc54
tdsparrow <sqallowlee@gmail.com>
2016-12-24 12:38:18 +0800

    fix ipython prompt
       after http://emacs.stackexchange.com/questions/24453/weird-shell-output-when-using-ipython-5?newreg=7e15e274404d40d3bf722e1e310ee278http://emacs.stackexchange.com/questions/24453/weird-shell-output-when-using-ipython-5?newreg=7e15e274404d40d3bf722e1e310ee278

--- el-setup/init-elpy.el
+++ el-setup/init-elpy.el
@@ -1,6 +1,9 @@
 (require 'elpy)
 (elpy-enable)
 (when (executable-find "ipython")
-    (elpy-use-ipython))
+  (elpy-use-ipython))
+
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i")
 
 (provide 'init-elpy)
