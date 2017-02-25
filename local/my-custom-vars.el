9f85241392e1ce2425db3a6b8ac5b6db64b62ed3
tdsparrow <sqallowlee@gmail.com>
2017-02-25 14:35:03 +0800

    upgrade elpy

--- local/my-custom-vars.el
+++ local/my-custom-vars.el
@@ -1,5 +1,5 @@
 (require 'my-local-vars nil 'noerror)
-(setq my-pkg-list '(go-mode helm yaml-mode monokai-theme elpy helm-gtags web-mode))
+(setq my-pkg-list '(go-mode go-company helm yaml-mode monokai-theme s elpy helm-gtags web-mode))
 
 (add-hook 'isearch-update-post-hook 'redraw-display)
 
