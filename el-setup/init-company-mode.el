f1ccc51bc1a1374121b360a013b8be4a5d23c987
tdsparrow <sqallowlee@gmail.com>
2018-01-01 11:30:25 +0800

    add yas

--- el-setup/init-company-mode.el
+++ el-setup/init-company-mode.el
@@ -1,5 +1,6 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+(add-to-list 'company-backends '(company-anaconda :with company-capf))
 
 (provide 'init-company)
 
--- local/my-custom-vars.el
+++ local/my-custom-vars.el
@@ -1,7 +1,7 @@
 (require 'my-local-vars nil 'noerror)
-(setq my-pkg-list '(go-mode go-company helm yaml-mode monokai-theme
+(setq my-pkg-list '(go-mode go-company helm yaml-mode monokai-theme yasnippet
 			    s anaconda-mode pyvenv helm-gtags web-mode tide
-			    virtualenvwrapper flycheck))
+			    company-anaconda virtualenvwrapper flycheck))
 
 (add-hook 'isearch-update-post-hook 'redraw-display)
 
