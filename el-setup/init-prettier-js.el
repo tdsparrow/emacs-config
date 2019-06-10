4864ce8e2c95d8703bc0f14ff17660456fd11d10
tdsparrow <sqallowlee@gmail.com>
2019-06-10 16:07:49 +0800

    add prettier js

---
+++ el-setup/init-prettier-js.el
@@ -0,0 +1,3 @@
+(add-hook 'js-jsx-mode-hook 'prettier-js-mode)
+(add-hook 'js-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
--- local/my-custom-vars.el
+++ local/my-custom-vars.el
@@ -1,6 +1,6 @@
 (require 'my-local-vars nil 'noerror)
 (setq my-pkg-list '(go-mode helm yaml-mode monokai-theme yasnippet my-go-company py-yapf
-			    s anaconda-mode pyvenv helm-gtags web-mode tide
+			    s anaconda-mode pyvenv helm-gtags web-mode tide gotest prettier-js
 			    company-anaconda virtualenvwrapper flycheck auto-virtualenv))
 
 (add-hook 'isearch-update-post-hook 'redraw-display)
