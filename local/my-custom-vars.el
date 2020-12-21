81ca013cde4e5ccad0a3b379f4dcdd7dc489a908
liren <liren@gjzq.com.cn>
2020-12-21 16:09:03 +0800

    add lsp and other modes

--- local/my-custom-vars.el
+++ local/my-custom-vars.el
@@ -1,6 +1,6 @@
 (require 'my-local-vars nil 'noerror)
-(setq my-pkg-list '(go-mode helm yaml-mode monokai-theme yasnippet py-yapf
-			    s anaconda-mode pyvenv helm-gtags web-mode tide gotest prettier-js
+(setq my-pkg-list '(go-mode helm yaml-mode monokai-theme yasnippet py-yapf php-mode-improved quelpa cucumber
+			    s anaconda-mode pyvenv helm-gtags web-mode tide gotest prettier-js lsp-mode
 			    company-anaconda virtualenvwrapper flycheck auto-virtualenv))
 
 (add-hook 'isearch-update-post-hook 'redraw-display)
