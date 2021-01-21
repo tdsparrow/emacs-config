13950c6b43cfcb5d02e502a1c22299b97648ea32
tdsparrow <sqallowlee@gmail.com>
2021-01-22 05:46:36 +0800

    update lsp conf

--- el-setup/init-lsp-mode.el
+++ el-setup/init-lsp-mode.el
@@ -1,16 +1,12 @@
 (require 'lsp-mode)
-(require 'lsp-ui)
 
 (add-to-list 'load-path (expand-file-name "clients" (el-get-package-directory 'lsp-mode)))
 
 (add-hook 'js-mode-hook #'lsp 100)
 
 (setq lsp-log-io t)
-(setq lsp-ui-sideline-show-hover t)
 
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-
+(require 'js-mode)
 (define-key js-mode-map [(meta ?.)] #'xref-find-definitions)
 
 (provide 'init-lsp-mode)
